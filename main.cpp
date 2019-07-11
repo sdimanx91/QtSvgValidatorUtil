95291c3333920d6a9096529d003b77ebe2ef17ed
Дмитрий Слободчиков <d.slobodchikov@2gis.ru>
2019-07-11 16:58:03 +0700

    initial

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <QtSvg/QSvgRenderer>
+#include <QtCore/QString>
+#include <QtCore/QFileInfo>
+#include <QtCore/QDebug>
+
+void printArgcHelp()
+{
+	qInfo() << "---------- using validator ----------";
+	qInfo() << "android_svg_validator <svgFileName>";
+}
+
+int badResult(const QString & msg)
+{
+	qWarning() << "ERROR: " << msg;
+	printArgcHelp();
+	return 1;
+}
+
+QString parseFileName(int argc, char *argv[])
+{
+	if (argc < 2) {
+		return QString();
+	}
+	return QString::fromStdString( argv[1] );
+}
+
+int main(int argc, char *argv[])
+{
+	const QString fileName = parseFileName(argc, argv);
+	if (fileName.isEmpty()) return
+		badResult(QStringLiteral("file name is not provided"));
+
+	const QFileInfo fileInfo = QFileInfo(fileName);
+	if (!fileInfo.exists())
+		return badResult(QStringLiteral("file is not exists"));
+	if (fileInfo.suffix() != QStringLiteral("svg"))
+		return badResult(QStringLiteral("Invalid file name suffix. Expected .svg."));
+
+	if (!QSvgRenderer(fileName).isValid())
+		return badResult(QStringLiteral("Invalid svg!"));
+
+	qInfo() << "SUCCESS: Svg file is valid";
+
+	return 1;
+}
